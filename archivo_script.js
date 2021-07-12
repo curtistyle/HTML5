1212381a96492769e717fde69b9bb86f69c26a4d
Sebastian Maldonado <58012659+curtistyle@users.noreply.github.com>
2021-07-12 12:13:08 -0300

    identificacion_de_elementos

--- archivo_script.js
+++ archivo_script.js
@@ -17,13 +17,31 @@ function ejecuta(){
     }
     */
 
+    /*
     for(var i=0;i<4;i++){
     var x = document.getElementsByClassName("importante")[i].onclick=bienvenida;
+    }
+    */
+
+    // document.querySelector(".importante").onclick=bienvenida;
+
+    // document.querySelector("#principal p:last-child").onclick=bienvenida;
+
+    /*
+    for(var i=0;i<4;i++){
+    var elementos = document.querySelectorAll("#principal p");
+    elementos[i].onclick=bienvenida;
+    }*/
+
+    var elementos=document.querySelectorAll("#principal p, span");
+
+    for (var i=0; i < elementos.length; i++){
+        elementos[i].onclick=bienvenida;
     }
 }
 
 function bienvenida(){
     alert("Bienvenidos")
 }
-
+// evento de ventana
 window.onload=ejecuta; //Cuando carga la ventana ejecuta la funcion 'bienvenida'
